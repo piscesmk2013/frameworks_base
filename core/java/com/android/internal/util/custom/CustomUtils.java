f2e9d5ec32a90cf58ebfbf52315d7279f3e90a4a
Roman Birg <romanbirg@gmail.com>
2016-10-23 06:43:28 +0200

    Utils: Add isPackageInstalled boolean
    Change-Id: Ibadea83b68c012c08e47c3622ba96a37c8ddf1b9
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- core/java/com/android/internal/util/custom/CustomUtils.java
+++ core/java/com/android/internal/util/custom/CustomUtils.java
@@ -27,6 +27,8 @@ import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.ContentObserver;
@@ -445,5 +447,24 @@ public class CustomUtils {
                 setSleepMode(enabled);
             }
         }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 }
