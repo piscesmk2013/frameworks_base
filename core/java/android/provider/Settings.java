5291183de2d096ee9f39ed34a28f9e7e27ef5ab5
Joe Maples <joe@frap129.org>
2018-01-03 21:53:01 -0500

    SystemUI: Smart Pixels [1/2]
    Disables a percentage of pixels on screen to reduce power consumption.
    If enabled with battery saver, don't scale brightness at 0.5f for UX.
    
    Includes:
     - Option to enable on battery saver
     - User chosen grid
     - Burn-in protection
    
    Configurable via overlay and disabled by defualt:
      "config_supportSmartPixels"
    
    Change-Id: Id3c78548cb090ab2da11f543da31c5a408fb9fe9
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5437,6 +5437,30 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Whether to enable Smart Pixels
+         * @hide
+         */
+        public static final String SMART_PIXELS_ENABLE = "smart_pixels_enable";
+
+        /**
+         * Smart Pixels pattern
+         * @hide
+         */
+        public static final String SMART_PIXELS_PATTERN = "smart_pixels_pattern";
+
+        /**
+         * Smart Pixels Shift Timeout
+         * @hide
+         */
+        public static final String SMART_PIXELS_SHIFT_TIMEOUT = "smart_pixels_shift_timeout";
+
+        /**
+         * Whether Smart Pixels should enable on power saver mode
+         * @hide
+         */
+        public static final String SMART_PIXELS_ON_POWER_SAVE = "smart_pixels_on_power_save";
+
+        /**
          * boolean value. toggles using arrow key locations on nav bar
          * as left and right dpad keys
          * @hide
