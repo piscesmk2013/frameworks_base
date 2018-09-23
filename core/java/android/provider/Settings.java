4c679589b98e2f228ecb886859e32a2c26932266
ezio84 <brabus84@gmail.com>
2018-09-24 00:21:01 +0530

    base: Option for Up, Down as well for Network Traffic [1/2]
    idoybh: Add settings value to backup array
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Change-Id: I99f1f8a2a770b6900bccdcc20eae301185b57224

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5898,6 +5898,13 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
+    	 * What to show in network traffic indicator in statusbar
+    	 * @hide
+    	 */
+        @Readable
+        public static final String NETWORK_TRAFFIC_TYPE = "network_traffic_type";
+
+        /**
          * Network traffic inactivity threshold (default is 1 kBs)
          * @hide
          */
@@ -6047,6 +6054,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OPTIMIZED_CHARGE_CEILING);
             PRIVATE_SETTINGS.add(OPTIMIZED_CHARGE_FLOOR);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_TYPE);
             PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
         }
 
--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -103,6 +103,7 @@ public class SystemSettings {
         Settings.System.RINGTONE_VIBRATION_PATTERN,
         Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
         Settings.System.NETWORK_TRAFFIC_STATE,
+        Settings.System.NETWORK_TRAFFIC_TYPE,
         Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD,
     };
 }
