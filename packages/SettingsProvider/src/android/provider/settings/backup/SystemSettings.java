4371035a07984b502fb55c147ef396c4d3594c0a
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-05 22:21:04 +0300

    [SQUASH]base: Allow choosing a custom vibration pattern [1/3]
    commit e9d414c58280ace5df5601bcddb0724f528f5b3b
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Fri Jun 5 22:21:04 2020 +0300
    
        base: Allow choosing a custom vibration pattern [1/3]
    
        Change-Id: I0d00f94b185a1032deb3e8413720d47a411cd6f7
    
    commit a29a2a4cdbcc5ceb7acad4d89c448e37ff8eeb56
    Author: Ido Ben-Hur <idoybh2@gmail.com>
    Date:   Mon Feb 22 05:55:26 2021 +0200
    
        base: Have a proper validator for custom ringtone vibration pattern
    
        [jhonboy121]: validate using a ListValidator
    
        Change-Id: I9657d76f4b961ac25fc52c9f6533549eda356386
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -96,5 +96,6 @@ public class SystemSettings {
         Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
         Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL,
         Settings.System.RINGTONE_VIBRATION_PATTERN,
+        Settings.System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
     };
 }
--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -238,6 +238,19 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.DOUBLE_TAP_SLEEP_LOCKSCREEN, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.DOUBLE_TAP_SLEEP_GESTURE, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.STATUS_BAR_BRIGHTNESS_CONTROL, BOOLEAN_VALIDATOR);
-        VALIDATORS.put(System.RINGTONE_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 4));
+        VALIDATORS.put(System.RINGTONE_VIBRATION_PATTERN, new InclusiveIntegerRangeValidator(0, 5));
+        VALIDATORS.put(System.CUSTOM_RINGTONE_VIBRATION_PATTERN,
+                 new ListValidator(",") {
+
+                    @Override
+                    protected boolean isEntryValid(String entry) {
+                        return entry != null;
+                    }
+
+                    @Override
+                    protected boolean isItemValid(String item) {
+                        return new InclusiveIntegerRangeValidator(0, 1000).validate(item);
+                    }
+                });
     }
 }
