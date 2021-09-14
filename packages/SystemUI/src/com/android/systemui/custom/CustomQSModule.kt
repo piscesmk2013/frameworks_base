e525624d447a762876c50ab0ef5e9e44a110e70c
jhonboy121 <alfredmathew05@gmail.com>
2021-09-14 20:23:04 +0530

    [SQUASH] SystemUI: introduce refresh rate tile
    * supports cycling between 60 to peak refresh rate and auto mode
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomQSModule.kt
@@ -15,6 +15,7 @@ import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.AntiFlickerTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.SleepModeTile
+import com.android.systemui.qs.tiles.RefreshRateTile;
 
 import dagger.Binds
 import dagger.Module
@@ -106,4 +107,10 @@ interface CustomQSModule {
     @IntoMap
     @StringKey(SleepModeTile.TILE_SPEC)
     fun bindSleepModeTile(sleepModeTile: SleepModeTile): QSTileImpl<*>
+
+    /** Inject refreshRateTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(RefreshRateTile.TILE_SPEC)
+    fun bindRefreshRateTile(refreshRateTile: RefreshRateTile): QSTileImpl<*>
 }
