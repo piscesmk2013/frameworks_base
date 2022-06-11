40e0e8e9830a12407745a04d3ee38775d50ef881
LibXZR <i@xzr.moe>
2022-06-11 16:13:53 +0800

    base: Add support for strict standby policy
    When dismissing from recents, this will force stop apps with restricted
    battery usage and put them into RESTRICTED standby bucket. Also all
    restrictions will be imposed even when device is charging.
    
    This can be useful to kill dirty vigorous Chinese apps.
    
    Change-Id: I8f075ef48c5775971e3e0b8b1ea843d70a2e44c4
    Signed-off-by: LibXZR <i@xzr.moe>

--- apex/jobscheduler/service/java/com/android/server/usage/AppStandbyController.java
+++ apex/jobscheduler/service/java/com/android/server/usage/AppStandbyController.java
@@ -490,6 +490,8 @@ public class AppStandbyController
     private PackageManager mPackageManager;
     Injector mInjector;
 
+    private boolean mStrictStandbyPolicyEnabled;
+
     private static class Pool<T> {
         private final T[] mArray;
         private int mSize = 0;
@@ -627,9 +629,27 @@ public class AppStandbyController
         }
     }
 
+    private void setStrictStandbyPolicyEnabled(boolean enabled) {
+        if (mStrictStandbyPolicyEnabled == enabled)
+            return;
+
+        synchronized (mAppIdleLock) {
+            final boolean oldParoleState = isInParole();
+            mStrictStandbyPolicyEnabled = enabled;
+            if (isInParole() != oldParoleState) {
+                postParoleStateChanged();
+            }
+        }
+    }
+
     @Override
     public boolean isAppIdleEnabled() {
         return mAppIdleEnabled;
+    }
+
+    @Override
+    public boolean isStrictStandbyPolicyEnabled() {
+        return mStrictStandbyPolicyEnabled;
     }
 
     @Override
@@ -829,7 +849,7 @@ public class AppStandbyController
 
     @Override
     public boolean isInParole() {
-        return !mAppIdleEnabled || mIsCharging;
+        return !mAppIdleEnabled || mIsCharging && !mStrictStandbyPolicyEnabled;
     }
 
     private void postParoleStateChanged() {
@@ -2926,6 +2946,8 @@ public class AppStandbyController
             // ADAPTIVE_BATTERY_MANAGEMENT_ENABLED is a user setting, so it has to stay in Settings.
             cr.registerContentObserver(Global.getUriFor(Global.ADAPTIVE_BATTERY_MANAGEMENT_ENABLED),
                     false, this);
+            cr.registerContentObserver(Global.getUriFor(Global.STRICT_STANDBY_POLICY),
+                    false, this);
             mInjector.registerDeviceConfigPropertiesChangedListener(this);
             // Load all the constants.
             // postOneTimeCheckIdleStates() doesn't need to be called on boot.
@@ -3126,6 +3148,10 @@ public class AppStandbyController
             synchronized (mAppIdleLock) {
                 mAllowRestrictedBucket = mInjector.isRestrictedBucketEnabled();
             }
+
+            setStrictStandbyPolicyEnabled(
+                    Global.getInt(mContext.getContentResolver(),
+                            Global.STRICT_STANDBY_POLICY, 0) == 1);
 
             setAppIdleEnabled(mInjector.isAppIdleEnabled());
         }
