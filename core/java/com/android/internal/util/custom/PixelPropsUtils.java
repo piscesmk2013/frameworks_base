5f3b4c4b4ce730c97d0f28bbeb932ec648d6f98c
minaripenguin <minaripenguin@users.noreply.github.com>
2023-02-08 22:36:56 +0530

    PixelPropUtils: Update gms unstable process spoof to walleye
    * spoof product & device name also to fix cts/play certification as of today
    
    original commit: https://github.com/minaripenguin/android_frameworks_base/commit/3714dd8b8c6ab1bd621ba23cf14d32dc01abd302
    
    Change-Id: Ia32c6a6366cea9e03b76bec48e849dfe43620d2f
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -378,12 +378,12 @@ public class PixelPropsUtils {
     }
 
     private static void spoofBuildGms() {
-        // Alter model name and fingerprint to avoid hardware attestation enforcement
-        setBuildField("FINGERPRINT", "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys");
-        setBuildField("PRODUCT", "marlin");
-        setBuildField("DEVICE", "marlin");
-        setBuildField("MODEL", "Pixel XL");
-        setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.N_MR1);
+        // Alter build parameters to pixel 2 for avoiding hardware attestation enforcement
+        setBuildField("DEVICE", "walleye");
+        setBuildField("FINGERPRINT", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+        setBuildField("MODEL", "Pixel 2");
+        setBuildField("PRODUCT", "walleye");
+        setVersionField("DEVICE_INITIAL_SDK_INT", Build.VERSION_CODES.O);
     }
 
     private static void setVersionField(String key, Object value) {
