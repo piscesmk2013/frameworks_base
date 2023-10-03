0e0277dff07e156dffe2f3f81fd32bf4f65f7868
someone5678 <someone5678@users.noreply.github.com>
2023-10-03 13:04:50 +0900

    SystemUI: reTicker: Import SystemUIToast icon retrieving logic and adapt it
    * Almost always get app icon
      including apps installed in work/multi profile
    
    Change-Id: Ia9957401e3920bfa313e3e7263e20d9e72990bea
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -17,6 +17,10 @@
 package com.android.systemui.shade;
 
 import static android.app.StatusBarManager.WINDOW_STATE_SHOWING;
+
+import static android.content.pm.ApplicationInfo.FLAG_SYSTEM;
+import static android.content.pm.ApplicationInfo.FLAG_UPDATED_SYSTEM_APP;
+
 import static android.view.MotionEvent.CLASSIFICATION_MULTI_FINGER_SWIPE;
 import static android.view.MotionEvent.CLASSIFICATION_TWO_FINGER_SWIPE;
 import static android.view.View.INVISIBLE;
@@ -56,6 +60,9 @@ import android.app.PendingIntent;
 import android.app.StatusBarManager;
 import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.database.ContentObserver;
@@ -77,6 +84,7 @@ import android.os.VibrationEffect;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 import android.text.TextUtils;
+import android.util.IconDrawableFactory;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
@@ -5176,6 +5184,30 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     }
 
     /* reTicker */
+    /**
+     * Get badged app icon if necessary, similar as used in the Settings UI.
+     * @return The icon to use
+     */
+    public static Drawable getBadgedIcon(Context context, String packageName,
+            int userId) {
+        try {
+            final PackageManager packageManager = context.getPackageManager();
+            final ApplicationInfo appInfo = packageManager.getApplicationInfoAsUser(
+                    packageName,
+                    PackageManager.ApplicationInfoFlags.of(PackageManager.GET_META_DATA),
+                    userId);
+            if (appInfo == null) {
+                return null;
+            }
+
+            IconDrawableFactory iconFactory = IconDrawableFactory.newInstance(context);
+            return iconFactory.getBadgedIcon(appInfo, UserHandle.getUserId(appInfo.uid));
+        } catch (NameNotFoundException e) {
+            Log.d(TAG, "Couldn't find application info for packageName=" + packageName
+                    + " userId=" + userId);
+            return null;
+        }
+    }
 
     public void reTickerView(boolean visibility) {
         if (!mReTickerStatus || mReTickerLandscapeOnly && !isLandscape()) return;
@@ -5185,18 +5217,16 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         String reTickerContent;
         if (visibility && getExpandedFraction() != 1) {
             mNotificationStackScroller.setVisibility(View.GONE);
-            StatusBarNotification sbn = mHeadsUpManager.getTopEntry().getRow().getEntry().getSbn();
+            StatusBarNotification sbn
+                = mHeadsUpManager.getTopEntry()
+                        .getRow().getEntry().getSbn();
             Notification notification = sbn.getNotification();
             String pkgname = sbn.getPackageName();
-            Drawable icon = null;
-            try {
-                if ("com.android.systemui".equals(pkgname)) {
-                    icon = mView.getContext().getDrawable(notification.icon);
-                } else {
-                    icon = mView.getContext().getPackageManager().getApplicationIcon(pkgname);
-                }
-            } catch (NameNotFoundException e) {
-            }
+            Drawable icon = getBadgedIcon(
+                        mView.getContext(),
+                        pkgname,
+                        sbn.getUser().getIdentifier()
+                    );
 
             String content = notification.extras.getString("android.text");
             if (TextUtils.isEmpty(content)) return;
